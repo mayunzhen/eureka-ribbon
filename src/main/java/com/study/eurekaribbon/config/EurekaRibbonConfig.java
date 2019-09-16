d96a88144a36c0dcdd6c87386f599ac808b2f35c
马云珍 <mayunzhensy@chinamobile.com>
2019-09-16 15:24:59 +0800

    add 熔断

--- src/main/java/com/study/eurekaribbon/config/EurekaRibbonConfig.java
+++ src/main/java/com/study/eurekaribbon/config/EurekaRibbonConfig.java
@@ -1,5 +1,7 @@
 package com.study.eurekaribbon.config;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,5 +22,13 @@ public class EurekaRibbonConfig {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 }
